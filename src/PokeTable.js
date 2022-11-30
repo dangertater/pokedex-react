e8676b063a81f230cc6938216f2741d94bccc9f6
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-30 16:22:56 -0700

    eric helped looking alright

--- src/PokeTable.js
+++ src/PokeTable.js
@@ -7,34 +7,54 @@ import TableRow from "@mui/material/TableRow"
 import Paper from "@mui/material/Paper"
 import { TableBody } from "@mui/material"
 
-let generateIndyPokemon = (name, type, startingAbilities, coolness) => {
-	return { name, type, startingAbilities, coolness }
+let generateIndyPokemon = (name) => {
+	return { name }
 }
-
+//TODO add the other
 export default function PokeTable(props) {
 	console.log("props.data.results[0] from PokeTable", props.data.results[0])
-	let rows = () => {
-		generateIndyPokemon(
-			props.data.results.map((indyRow) => {
-				console.log("indyRow from inside PokeTable", indyRow)
-			})
-		)
-	}
+
+	generateIndyPokemon(
+		props.data.results.map((indyPokemon) => {
+			let justNames = []
+			console.log("indyPokemon.name", indyPokemon.name)
+			justNames.push(indyPokemon.name)
+			console.log("justNames", justNames)
+			return indyPokemon.name
+		})
+	)
+
 	return (
 		<TableContainer component={Paper}>
 			<Table sx={{ minWidth: 350 }} aria-label="simple table">
 				<TableHead>
 					<TableRow>
 						<TableCell>Pokemon</TableCell>
-						<TableCell align="right">Type</TableCell>
+						{/* <TableCell align="right">Type</TableCell>
 						<TableCell align="right">Starting Abilities</TableCell>
-						<TableCell align="right">Coolness</TableCell>
+						<TableCell align="right">Coolness</TableCell> */}
 					</TableRow>
 				</TableHead>
 				<TableBody>
-					<TableCell align="right">{rows()}</TableCell>
+					<TableRow></TableRow>
 				</TableBody>
 			</Table>
 		</TableContainer>
 	)
 }
+
+// {
+/* <TableCell align="right">{rows()}</TableCell> */
+// }
+
+// {rows.map((row) => (
+//     <TableRow>
+//         <TableCell component="th" scope="row">
+//             {row.name}
+//         </TableCell>
+//         <TableCell align="right">{row.calories}</TableCell>
+//         <TableCell align="right">{row.fat}</TableCell>
+//         <TableCell align="right">{row.carbs}</TableCell>
+//         <TableCell align="right">{row.protein}</TableCell>
+//     </TableRow>
+// ))}
