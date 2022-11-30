e10c03e4948fb4d7dd200007ab99d02074ec23ba
dangertater <106702944+dangertater@users.noreply.github.com>
2022-11-30 16:38:14 -0700

    table is displayed with indy pokemon names in rows upon fetch button click

--- src/PokeTable.js
+++ src/PokeTable.js
@@ -6,40 +6,41 @@ import TableHead from "@mui/material/TableHead"
 import TableRow from "@mui/material/TableRow"
 import Paper from "@mui/material/Paper"
 import { TableBody } from "@mui/material"
-
 let generateIndyPokemon = (name) => {
 	return { name }
 }
 //TODO add the other
 export default function PokeTable(props) {
-	console.log("props.data.results[0] from PokeTable", props.data.results[0])
-
+	let justNames = []
 	generateIndyPokemon(
 		props.data.results.map((indyPokemon) => {
-			let justNames = []
 			console.log("indyPokemon.name", indyPokemon.name)
 			justNames.push(indyPokemon.name)
-			console.log("justNames", justNames)
 			return indyPokemon.name
 		})
 	)
-
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 350 }} aria-label="simple table">
-				<TableHead>
-					<TableRow>
-						<TableCell>Pokemon</TableCell>
-						{/* <TableCell align="right">Type</TableCell>
-						<TableCell align="right">Starting Abilities</TableCell>
-						<TableCell align="right">Coolness</TableCell> */}
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					<TableRow></TableRow>
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 350 }} aria-label="simple table">
+					<TableHead>
+						<TableRow>
+							<TableCell>Pokemon</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{justNames.map((name) => {
+							return (
+								<TableRow>
+									<TableCell>{name}</TableCell>
+								</TableRow>
+							)
+						})}
+						{/* <TableRow>{justNames}</TableRow> */}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</>
 	)
 }
 
